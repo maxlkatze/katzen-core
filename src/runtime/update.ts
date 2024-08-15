ba24729dd6a9f3130831d8d47104e5990c85e9ca
maxlkatze <git@cubedpixels.net>
2024-08-15 06:44:40 +0200

    fix(katzencore): Trying new updater

--- src/runtime/update.ts
+++ src/runtime/update.ts
@@ -1,12 +1,11 @@
+import { version } from '../../package.json'
+
 export interface UpdateResult {
   currentVersion: string
   latestVersion: string
 }
 
 export const updateCheck = async () => {
-  const pkg = (await import('../../package.json', { assert: { type: 'json' } })
-  ).default as { version: string }
-  const version = pkg.version
   const https = await import('node:https')
   return new Promise<UpdateResult>((resolve) => {
     https.get('https://registry.npmjs.org/@maxlkatze/cms/latest', (res) => {
